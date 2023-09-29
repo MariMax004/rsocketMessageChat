e77de70e05b3f81cea34eb83059b30ce6e514108
mariamaksimova <Maria.ms004@mail.ru>
2023-09-29 17:05:27 +0600

    server

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("org.springframework.boot:spring-boot-starter-webflux")
+    implementation ("io.rsocket:rsocket-core:0.12.1")
+    implementation ("io.rsocket:rsocket-transport-netty:0.12.1")
+    implementation("io.projectreactor.kafka:reactor-kafka:1.3.17")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
